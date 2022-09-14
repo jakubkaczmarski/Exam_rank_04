af6b4929fd51a6a7bfc79d3aba2f50f1447dd032
= <kuba.kaczmarski312@gmail.com>
2022-09-14 19:10:06 +0200

    explanation

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: jkaczmar <jkaczmar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/14 18:17:37 by jkaczmar          #+#    #+#             */
-/*   Updated: 2022/09/14 18:51:10 by jkaczmar         ###   ########.fr       */
+/*   Updated: 2022/09/14 19:09:51 by jkaczmar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+
+//Steps to make this work
+//Intialize variables 
+//we need a lastfd flag to check for the pipe
+//Then we need array of two ints for pipe itself
+//we are looping through all the argumetns passed by the user till it's smaller or equal
+//Then we there is somehting in a index and if it's different than "|" and ";" that means we have an argument
+//So we can increment the arguement count
+//If that's not the case but we already have argument count bigger than 0
+//We move to another if statement where we check if the current positon in argv is a pipe if it is we change the flag to 1
+//if not we change it to 0
+//Next we go to the execution part we check if our current argument isn't a cd
+//if it is we check if there are at least two arguments to it and execute it
+//If it's not cd we go to normal executing 
+//We check for pipe flag if pipe_flag is true we use pipe(pipe_fd);
+//After this we fork and save pid
+//If pid is negative we throw an error
+//If it's 0 we are in the child
+//in the child we check if the last_fd flag is different than -1 and we dup2(lastfd, 0)
+//Next up we check if our pipe flag is equal to 0 and if it is we dup2(pipe_fd[1],1)
+//Next up we just execute the command with execve
+//in the parent process we wait
+//and we close(pipe_fd[1])
+//if last_fd_flag is different than -1 we close it 
+//now if our pipe_flag is 1
+//we assign last_fd = pipe_fd[0]
+//if not we assign -1 to it 
 
 int main(int argc, char **argv, char **env)
 {
